cd88e2d11aed4ef73094114e718dad856d38e864
Hasitha Gamage <hasitha@rocketbots.io>
2023-05-11 17:37:20 +0800

    fix: Create response body

--- dist/index.js
+++ dist/index.js
@@ -9796,19 +9796,30 @@ const main = async () => {
     try {
         const slackWebhookUrl = core.getInput('slack-webhook-url', { required: true });
 
-        const { 
-            context: { payload: contextPayload, eventName },
-            event
+        const {
+            context: {
+                eventName,
+                payload: {
+                    pull_request: {
+                        number: pullRequestNumber,
+                        title: pullRequestTitle,
+                        body: pullRequestBody,
+                        html_url: pullRequestUrl,
+                        merged_by: {
+                            login: pullRequestMergedBy
+                        }
+                    }
+                },
+            }
         } = github;
 
-        const [ owner, repo ] = process.env.GITHUB_REPOSITORY.split('/');
+        const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
 
         if (eventName !== 'pull_request' || contextPayload.pull_request === undefined || contextPayload.action !== 'closed' || contextPayload.pull_request.merged !== true || contextPayload.pull_request.draft === true) {
             console.log('ERROR :: This action should only be run on a closed pull request that has been merged');
             process.exit(1);
         }
 
-        const { number: pullRequestNumber, title: pullRequestTitle, body: pullRequestBody } = contextPayload.pull_request;
 
         console.log(`pullRequestNumber >> `, pullRequestNumber);
         console.log(`pullRequestTitle >> `, pullRequestTitle);
@@ -9816,6 +9827,17 @@ const main = async () => {
         console.log(`slackWebhookUrl >> `, slackWebhookUrl);
         console.log(`Context >> `, JSON.stringify(contextPayload));
         console.log(`Event >> `, JSON.stringify(event));
+
+        const responseBody = {
+            repo,
+            actor: pullRequestMergedBy,
+            message: pullRequestBody,
+            pr_number: pullRequestNumber,
+            pr_title: pullRequestTitle,
+            pr_url: pullRequestUrl
+        };
+
+        console.log('Response Body >> ', JSON.stringify(responseBody));
 
     } catch (error) {
         core.setFailed(error.message);
